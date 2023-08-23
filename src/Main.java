0485480857a080c430a3486a0e7cd97b0f1c8e24
lokytech5 <lokytech5@gmail.com>
2023-08-23 12:14:20 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String inputMessage = JOptionPane.showInputDialog("Enter a number to determine if it's fizz or buzz");
+        int number = Integer.parseInt(inputMessage);
+
+        if (number % 3 == 0 && number % 5 == 0) {
+            JOptionPane.showMessageDialog(null, "The number is FizzBuzz");
+        } else if (number % 3 == 0) {
+            JOptionPane.showMessageDialog(null, "The number is Fizz");
+        } else if (number % 5 == 0) {
+            JOptionPane.showMessageDialog(null, "The number is Buzz");
+        } else {
+            JOptionPane.showMessageDialog(null, "The number is neither Fizz nor Buzz: " + number);
+        }
+    }
+}
